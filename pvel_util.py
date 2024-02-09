4c0b7b9be0e784c143d2e2f9af055f05a0ca4580
mnky9800n <johnm.aiken@gmail.com>
2024-02-09 15:31:00 +0100

    there is a giant bug in the codes that nmeeds to be cleaned up

---
+++ pvel_util.py
@@ -0,0 +1,169 @@
+import numpy as np
+import obspy
+from hydrophone_data_processing import load, useful_variables, plotting, signal_processing
+import scipy.signal as signal
+import pandas as pd
+import matplotlib.dates as dates
+import obspy.signal.trigger as trigger
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import config
+from event_pipeline import Event
+from obspy.imaging.spectrogram import spectrogram
+from event_pipeline import Event
+import torchaudio.transforms as T
+import librosa
+import torch
+import matplotlib.dates as mdates
+import datetime
+
+vels = []
+depths = []
+ptimes = []
+
+def import_data(day, starttime, endtime):
+    daypaths = useful_variables.make_hydrophone_data_paths(borehole='a', year=2019, julian_day=day)
+    starttime = obspy.UTCDateTime(starttime)
+    endtime = obspy.UTCDateTime(endtime)
+    data = load.import_corrected_data_for_single_day(daypaths)
+    data.trim(starttime=starttime, endtime=endtime)
+    return data
+
+# def import_catalog(file):
+#     df = pd.read_csv(file)
+#     df.sort_values(by='first_arrival', inplace=True)
+#     df['arrival_datetime'] = df.arrival_time.apply(dates.num2date)
+#     return df
+
+def import_catalog(file):
+    columns_to_keep = ['id', 'depth', 'relative_depth',
+        'first_hydrophone', 'second_hydrophone',
+       'arrival_time', 'first_arrival', 'second_arrival', 'dt',
+       'max_amp', 'cum_amp', 'arrival_datetime', 'origin_time']
+    df = pd.read_csv(file)
+    df.sort_values(by='first_arrival', inplace=True)
+    df['arrival_datetime'] = df.arrival_time.apply(dates.num2date)
+    df = df[columns_to_keep].copy()
+    df.sort_values(by='arrival_datetime', inplace=True)
+    # # removing the events with maximum amplitude <0.1 since they are false positives
+    df = df[df.max_amp > 0.1].copy()
+    df.drop_duplicates(subset=['arrival_datetime',], inplace=True)
+    return df
+
+
+def get_event(event_id, day_number, df, hanning=True):
+    paths = useful_variables.make_hydrophone_data_paths(borehole='a', year=2019, julian_day=day_number)
+    waveforms = load.import_corrected_data_for_single_day(paths=paths)
+    df = df.copy()
+    e = Event(id=event_id
+              , starttime=df.arrival_time.loc[event_id]
+              , init_first_hphone=df.first_hydrophone
+              , waveforms=waveforms
+              , hanning=hanning
+             )
+    return e
+
+def get_times(event):
+    trace = event.stream[event.first_hydrophone_id]
+    times = trace.times()
+    times_len = times.shape[0]
+    return np.arange(0, times_len, 1)
+
+def plot_spectrogram(event, ax):
+    trace = event.stream[event.first_hydrophone_id]
+    torch_data = torch.from_numpy(trace.data)
+    spectrogram = T.Spectrogram(n_fft=2**6, hop_length=1)
+    spec = spectrogram(torch_data)
+    ax.imshow(librosa.power_to_db(spec), origin='lower', aspect='auto', cmap='gist_rainbow')
+    
+
+def plot_50hz_highpass(event, ax):
+    trace = event.stream[event.first_hydrophone_id].copy()
+    times = get_times(event=event)
+    tr = trace.filter(type='highpass', freq=50)
+    ax.plot(times, tr.data, color='violet', label='50Hz highpass'
+            ,linewidth=2)
+
+def plot_aic_pick(event, ax):
+    times = get_times(event=event)
+    # its 0.4 because its 2/5 because its 0.2s before and 0.3 after
+    aic_time = times[int(0.4*times.shape[0])]
+    ax.vlines(x=aic_time, ymin=-0.5, ymax=0.7, color='cyan'
+              ,linewidth=3, label='AIC pick')
+
+def plot_p(event, ax):
+    trace = event.stream[event.first_hydrophone_id]
+    times = get_times(event=event)
+    tr = trace.filter(type='highpass', freq=50)
+    
+    # find max/min peak
+    start = 180
+    end = 199
+    if np.max(tr.data[start:end]) > np.min(tr.data[start:end]):
+        ploc_t = times[np.argmax(tr.data[start:end]) + start]
+        ploc_y = np.max(tr.data[start:end])
+    else:
+        ploc_t = times[np.argmin(tr.data[start:end]) + start]
+        ploc_y = np.min(tr.data[start:end])
+    
+    # ax.plot(times, tr.data, color='cyan', alpha=0.5, label='what is this?')
+    print(ploc_t, ploc_y)
+    print(tr.data.shape)
+    ax.plot(ploc_t, ploc_y, marker='*', linestyle='', color='red', label='p-arrival pick')
+
+def plot_aic(event, ax):
+    times = get_times(event=event)
+    AICs = event.aics[event.first_hydrophone_id]
+    ax.plot(times, AICs, color='cyan', linewidth=2.5)
+
+def plot_aic_diff(event, ax):
+    times = get_times(event=event)
+    AICs = event.aics[event.first_hydrophone_id]
+    aic_diffs = np.diff(AICs)
+    ax.plot(times[1:], aic_diffs, color='magenta')
+
+def plot_event(event_id, day_number, df):
+    
+    e = get_event(event_id=event_id, day_number=day_number, df=df)
+    
+    fig, ax = plt.subplots(3, 1, figsize=(15, 8), sharex=True)
+
+    plot_50hz_highpass(event=e, ax=ax[0])
+    
+    plot_aic_pick(event=e,  ax=ax[0])
+
+    plot_p(event=e, ax=ax[0])
+    
+    plot_aic(event=e, ax=ax[1])
+    
+    ax2 = ax[1].twinx()
+    
+    plot_aic_diff(event=e, ax=ax2)
+    
+    plot_spectrogram(event=e, ax=ax[2])
+    
+    ax[0].set_ylabel('Pressure (Pa)', fontsize=15)
+    ax[0].legend(fontsize=15, loc='upper right')
+    ax[1].set_ylabel('AIC', fontsize=15, color='magenta')
+    ax2.set_ylabel('diff(AIC)', fontsize=15, color='cyan')
+    ax[2].set_ylabel('Hz', fontsize=15)
+    ax[2].set_yticks(np.arange(0, 32, 5))
+    ax[2].set_yticklabels(np.arange(0, 32, 5) * 500 / 32)
+    fig.tight_layout()
+
+def plot_event_for_p_pick(event_id, ptime, day_number, catalog):
+    fig, ax = plt.subplots(figsize=(15, 5))
+    # e = get_event(event_id=event_id, day_number=141, df=catalog)
+    e = get_event(event_id=event_id, day_number=day_number, df=catalog)
+    plot_50hz_highpass(event=e, ax=ax)
+    plot_aic_pick(event=e, ax=ax)
+    ax.arrow(x=ptime*1e3, y=0.75, dx=0, dy=-0.6, color='red', head_width=1, head_length=0.05)
+    # print('relative depth:', e.relative_depth)
+    print('origin time:', catalog.origin_time[event_id])
+    vel = e.relative_depth/(obspy.UTCDateTime(catalog.origin_time[event_id]) - ((e.starttime - 0.2) + ptime))
+    print('p velocity:', vel)
+    vels.append(vel)
+    depths.append(e.depth)
+    ptimes.append(0.2 - ptime)
